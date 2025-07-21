ceef45ce175d60fb6937d485066c018f96792bae
Shinto272000 <shintoshaju2018@gmail.com>
2025-07-21 22:57:08 +0530

    forth commit

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,5 +1,5 @@
 import Product from '../models/Product.js';
-import cloudinary from '../Config/cloudinary.js';
+import cloudinary from '../config/cloudinary.js';
 
 export const getAllProducts = async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import connectDB from './Config/database.js';
+import connectDB from './config/database.js';
 import productRoutes from './routes/productRoutes.js';
 // import connectDB from './config/database.js';
 
